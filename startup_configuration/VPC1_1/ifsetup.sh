6e91833b504eecea507f419d2074a4e1a362461b
Mizuto Arakawa <manchestersh0320@gmail.com>
2025-08-14 12:48:50 +0900

    Add yml & configuration file

---
+++ startup_configuration/VPC1_1/ifsetup.sh
@@ -0,0 +1,20 @@
+#!/bin/ash
+
+# 設定するインターフェースとIPアドレス
+INTERFACE="eth1"
+IP_ADDRESS="100.50.80.1"
+SUBNET_MASK="255.255.255.252"
+BROADCAST_ADDRESS="100.50.80.3"
+GATEWAY_ADDRESS="100.50.80.2"
+
+# インターフェースにIPアドレスとブロードキャストアドレスを設定
+echo "Setting IP address $IP_ADDRESS/$SUBNET_MASK and broadcast $BROADCAST_ADDRESS on $INTERFACE"
+ip addr add $IP_ADDRESS/$SUBNET_MASK broadcast $BROADCAST_ADDRESS dev $INTERFACE
+
+# インターフェースを有効化
+echo "Bringing up $INTERFACE"
+ip link set $INTERFACE up
+
+# デフォルトゲートウェイの設定
+ip route del default
+ip route add default via $GATEWAY_ADDRESS dev $INTERFACE
